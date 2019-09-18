f216ef776c491a0808e83cdb94258ed0528c2cce
David Lozzi <lozzi.david@bcg.com>
2019-09-18 12:46:48 -0400

    done and done

--- config.md
+++ config.md
@@ -1,8 +1,77 @@
-To use the config, create a config.py file, and copy/paste the following:
+# PCO Group Member Exports Overview
+
+The goal for this project is to easily export members of all active groups to perform some analysis and reporting on the data.
+
+This python script:
+1. Gets all active groups
+2. Gets the members for each group
+3. Gets the person data for each member
+4. Gets the geolocation of the person (using Google APIs)
+5. Performs a distance calculation for the members in the group
+6. Exports it all to a CSV
+
+The CSV will include the following columns:
+ * First Name ![PCO People][people]
+ * Last Name ![PCO People][people]
+ * Gender ![PCO People][people]
+ * Birthdate ![PCO People][people]
+ * Location ![PCO People][people]
+ * Location Long ![Google Maps][maps]
+ * Location Lat ![Google Maps][maps]
+ * Distance to Group
+ * Marital Status ![PCO People][people]
+ * Membership ![PCO People][people]
+ * Status ![PCO People][people]
+ * Person Record Updated ![PCO People][people]
+ * Person Record Created ![PCO People][people]
+ * Joined Group At ![PCO People][group]
+ * Role ![PCO People][group]
+ * Group ![PCO People][group]
+ * Group Location ![PCO People][group]
+ * Group Long ![PCO People][group]
+ * Group Lat ![PCO People][group]
+
+[people]: images/people.png "PCO People"
+[group]: images/groups.png "PCO Groups"
+[maps]: images/maps.png "Google Maps"
+
+# Setup
+
+## Install Libraries
+
+Before we begin, you'll need Python installed, obviously ;)
+
+Install the following libraries:
+
+```
+pip install requests
+pip install geopy
+```
+
+Learn more about [requests](https://2.python-requests.org/en/master/) and [geopy](https://geopy.readthedocs.io/en/stable/#module-geopy.distance).
+
+## Setup Configuration
+
+You have to create your own `config.py` file to store your API keys. Don't worry, this file is not included in the repo. Copy and paste the following into your config file:
 
 ```
 API_USERNAME = 'specify your user name'
 API_PASSWORD = 'specify your password'
+GOOGLE_APIKEY = 'specify your Google API key'
 ```
 
-The config.py file is ignored by git.
+Learn how to obtain a [Google API Key](https://developers.google.com/maps/documentation/geocoding/get-api-key).
+
+_**NOTE** The config.py file is ignored by git._
+
+# Using the Script
+
+Simply run the `exportGroupMembers.py`
+
+```
+python exportGroupMembers.py
+```
+
+As it runs, it'll share each group name and member name in the console, along with each API call being made, just for reference.
+
+This script will export the contents to `groupsExport.csv`. This file is not included in the git repo.
--- exportGroupMembers.py
+++ exportGroupMembers.py
@@ -1,9 +1,12 @@
-import requests # https://2.python-requests.org/en/master/
+import requests
+from geopy import distance
 import config
+import time
 
 # APIS
 class api:
   def get(self, url):
+    print("        " + url)
     resp = requests.get(
       url,
       auth=(config.API_USERNAME, config.API_PASSWORD)
@@ -17,43 +20,124 @@ class api:
 
 class groups:
   groupUrl = 'https://api.planningcenteronline.com/groups/v2/groups'
+  groupId = ""
+  locationUrl = ""
 
+  def __init__(self, grpId):
+    self.groupId = grpId
   def getAll(self):
     return api().get('%s?per_page=100' % (self.groupUrl))
-  def getMembers(self, groupId):
-    return api().get('%s/%s/memberships' % (self.groupUrl, groupId))
+  def getDetails(self):
+    resp = api().get('%s/%s' % (self.groupUrl, self.groupId))
+    self.locationUrl = resp["data"]["links"]["location"]
+    return resp
+  def getMembers(self):
+    return api().get('%s/%s/memberships' % (self.groupUrl, self.groupId))
+  def getLocation(self):
+    if self.locationUrl == None:
+      return None
+    else:
+      return api().get(self.locationUrl)
 
 class people:
   peopleUrl = 'https://api.planningcenteronline.com/people/v2/people'
+  peopleId = ''
+  maritalUrl = ''
+  addressUrl = ''
 
-  def getPerson(self, peopleId):
-    return api().get('%s/%s' % (self.peopleUrl, peopleId))
+  def __init__(self, peepId):
+    self.peopleId = peepId
+  def getPerson(self):
+    resp = api().get('%s/%s' % (self.peopleUrl, self.peopleId))
+    self.maritalUrl = resp["data"]["links"]["marital_status"]
+    self.addressUrl = resp["data"]["links"]["addresses"]
+    return resp
+  def getMaritalStatus(self):
+    if self.maritalUrl == None:
+      return None
+    else:
+      return api().get(self.maritalUrl)
+  def getAddress(self):
+    if self.addressUrl == None:
+      return None
+    else :
+      return api().get(self.addressUrl)
 
-# MODELS
-class member:
-  firstname = "david"
-  lastname = "lozzi"
+class maps:
+  mapUrl = 'https://maps.googleapis.com/maps/api/geocode/json?key=%s' % (config.GOOGLE_APIKEY)
+  address = ''
+  addresses = {}
 
-  def set(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
-    return self
+  def __init__(self, address):
+    self.address = address
+  def getLocation(self):
+    if self.address not in self.addresses:
+      resp = api().get('%s&address=%s' % (self.mapUrl, self.address))
+      self.addresses[self.address] = resp["results"][0]["geometry"]["location"]# expects { "lat": 00.000, "lng": 00.000 }
+    
+    return self.addresses[self.address]
 
-  def fullname(self):
-    return self.firstname + " " + self.lastname
 
+outputFile = open("groupsExport.csv", "w+")
+# add new columns to csvHeader AND update the number of columns in csvPlaceholder
+csvHeader = "First Name, Last Name, Gender, Birthdate, City, State, Zip, Location Long, Location Lat, Distance to Group, Marital Status, Membership, Status, Person Record Updated, Person Record Created, Joined Group At, Role, Group, Group Location, Group Long, Group Lat\r\n"
+csvPlaceholder = ("\"%s\"," * 21) + "\r\n"
+outputFile.write(csvHeader)
 
+groupsList = groups('').getAll()
 
-groupsList = groups().getAll()
-
-for group in groupsList["data"]:
-  print(group["attributes"]["name"])
-  members = groups().getMembers(group["id"])
+for grp in groupsList["data"]:
+  time.sleep(.4) # slowing down API calls to not exceed rates of 100 in 20s
+  print(grp["attributes"]["name"])
+  groupObj = groups(grp["id"])
+  groupDetail = groupObj.getDetails()
+  members = groupObj.getMembers()
+  grpLocation = groupObj.getLocation()
+  if grpLocation != None:
+    groupAddress = grpLocation["data"]["attributes"]["full_formatted_address"].replace("\n", ", ")
+    groupLong = grpLocation["data"]["attributes"]["longitude"]
+    groupLat = grpLocation["data"]["attributes"]["latitude"]
 
   for member in members["data"]:
     # try:
-    person = people().getPerson(member["attributes"]["account_center_identifier"])
-    print("    %s %s %s" % (member["attributes"]["first_name"], member["attributes"]["last_name"], person["data"]["attributes"]["gender"]))
+      print("    %s %s" % (member["attributes"]["first_name"], member["attributes"]["last_name"]))
+      time.sleep(.2) # a little more slow down
+      personObj = people(member["attributes"]["account_center_identifier"])
+      person = personObj.getPerson()
+      maritalStatus = personObj.getMaritalStatus()
+      addresses = personObj.getAddress()
+      if addresses != None and len(addresses["data"]) > 0:
+        address = addresses["data"][0]
+        mapApi = maps("%s %s %s" % (address["attributes"]["city"] if address["attributes"]["city"] != None else '',
+          address["attributes"]["state"] if address["attributes"]["state"] != None else '',
+          address["attributes"]["zip"] if address["attributes"]["zip"] != None else ''))
+        mapLocation = mapApi.getLocation()
+        memberDistance = distance.distance((groupLat, groupLong),(mapLocation["lat"], mapLocation["lng"])).miles
+
+      outputFile.write(csvPlaceholder % (
+        person["data"]["attributes"]["first_name"],
+        person["data"]["attributes"]["last_name"],
+        person["data"]["attributes"]["gender"],
+        person["data"]["attributes"]["birthdate"],
+        address["attributes"]["city"] if address["attributes"]["city"] != None else '',
+        address["attributes"]["state"] if address["attributes"]["state"] != None else '',
+        address["attributes"]["zip"] if address["attributes"]["zip"] != None else '',
+        mapLocation["lng"],
+        mapLocation["lat"],
+        memberDistance,
+        maritalStatus["data"]["attributes"]["value"] if maritalStatus != None else '',
+        person["data"]["attributes"]["membership"],
+        person["data"]["attributes"]["status"],
+        person["data"]["attributes"]["updated_at"],
+        person["data"]["attributes"]["created_at"],
+        member["attributes"]["joined_at"],
+        member["attributes"]["role"],
+        groupDetail["data"]["attributes"]["name"],
+        groupAddress,
+        groupLong,
+        groupLat
+        ))
     # except:
-    #   print("people errored for %s %s" % (member["attributes"]["first_name"], member["attributes"]["last_name"]))
-    
+    #   print("errored for %s %s" % (member["attributes"]["first_name"], member["attributes"]["last_name"]))
+
+outputFile.close()
